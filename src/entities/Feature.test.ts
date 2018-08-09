2371db7ec6f973660c1701bc511f0847c7a55dcc
Allain Lalonde <allain.lalonde@gmail.com>
2018-08-08 23:00:53 -0400

    phase1 - Feature Entity

--- src/entities/Feature.test.ts
+++ src/entities/Feature.test.ts
@@ -4,4 +4,35 @@ describe('Feature', () => {
   it('can be created', () => {
     expect(new Feature()).toBeInstanceOf(Feature);
   });
+
+  it('has expected properties', () => {
+    expect(Object.keys(new Feature())).toEqual([
+      'id',
+      'title',
+      'description',
+      'createdAt',
+    ]);
+  });
+
+  it('has expected default values', () => {
+    const f = new Feature();
+    expect(f.id).toBeNull();
+    expect(f.title).toBeNull();
+    expect(f.description).toBeNull();
+    expect(typeof f.createdAt).toEqual('number');
+  });
+
+  it('supports setting props from constructor', () => {
+    const f = new Feature({
+      id: '1',
+      title: 'Testing',
+      description: 'Description',
+      createdAt: 100000000,
+    });
+
+    expect(f.id).toEqual('1');
+    expect(f.title).toEqual('Testing');
+    expect(f.description).toEqual('Description');
+    expect(f.createdAt).toEqual(100000000);
+  });
 });
--- src/entities/Feature.ts
+++ src/entities/Feature.ts
@@ -1,1 +1,19 @@
-export default class Feature {}
+type FeatureSpec = {
+  id?: string;
+  title?: string;
+  description?: string;
+  createdAt?: number;
+};
+
+const DEFAULT_PROPS = { id: null, title: null, description: null };
+
+export default class Feature {
+  public id: string;
+  public title: string;
+  public description: string;
+  public createdAt: number;
+
+  constructor(props: FeatureSpec = DEFAULT_PROPS) {
+    Object.assign(this, { ...props, createdAt: props.createdAt || Date.now() });
+  }
+}
